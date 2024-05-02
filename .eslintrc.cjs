3a3463119475b678ee78778effbc6a3204a6b286
vishal-kacha <vishaldineshbhaikacha@gmail.com>
2024-05-02 20:30:00 +0530

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,43 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx"],
+      },
+    },
+  },
+  plugins: ["react-refresh", "react", "import", "jsx-a11y"],
+  rules: {
+    "react/jsx-no-target-blank": "off",
+    "react/prop-types": 0,
+    "react/react-in-jsx-scope": 0,
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    commonjs: true / false,
+    amd: true / false,
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Starter Template</h1>
+    </>
+  );
+}
+
+export default App;
